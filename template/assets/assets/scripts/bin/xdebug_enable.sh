19e1772abd534b89d47ebe22a9d87316f345721c
Martin Weinmayr <mw@dasistweb.de>
2020-10-28 15:55:42 +0100

    #16 fixed not wokring restart of fpm on xdebug-off
    Now both commands use the restart command instead of start

--- template/assets/assets/scripts/bin/xdebug_enable.sh
+++ template/assets/assets/scripts/bin/xdebug_enable.sh
@@ -6,7 +6,5 @@ sudo mv /etc/php/${PHP_VERSION}/cli/conf.d/20-xdebug.ini_disabled /etc/php/${PHP
 sudo sed -i 's/__dockware_host__/'${XDEBUG_REMOTE_HOST}'/g' /etc/php/${PHP_VERSION}/fpm/conf.d/20-xdebug.ini
 sudo sed -i 's/__dockware_host__/'${XDEBUG_REMOTE_HOST}'/g' /etc/php/${PHP_VERSION}/cli/conf.d/20-xdebug.ini
 
-sudo service php${PHP_VERSION}-fpm stop > /dev/null 2>&1 &
-sudo service php${PHP_VERSION}-fpm start
-
+sudo service php${PHP_VERSION}-fpm restart
 echo "------------------------------------------------"
