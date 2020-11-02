4ac47e076acd0a8c9e8521ba4e3613132cd54c36
Martin Weinmayr <mw@dasistweb.de>
2020-11-02 14:52:34 +0100

    fix xdebug start/stop
    as the command only workd on second execution the restart command helps out

--- template/assets/assets/scripts/bin/xdebug_disable.sh
+++ template/assets/assets/scripts/bin/xdebug_disable.sh
@@ -3,6 +3,5 @@ echo "DOCKWARE: deactivating Xdebug..."
 sudo mv /etc/php/${PHP_VERSION}/fpm/conf.d/20-xdebug.ini /etc/php/${PHP_VERSION}/fpm/conf.d/20-xdebug.ini_disabled  > /dev/null 2>&1 &
 sudo mv /etc/php/${PHP_VERSION}/cli/conf.d/20-xdebug.ini /etc/php/${PHP_VERSION}/cli/conf.d/20-xdebug.ini_disabled   > /dev/null 2>&1 &
 
-sudo service php${PHP_VERSION}-fpm stop > /dev/null 2>&1 &
-sudo service php${PHP_VERSION}-fpm start
+sudo service php${PHP_VERSION}-fpm restart > /dev/null 2>&1 &
 echo "-----------------------------------------------------------"
