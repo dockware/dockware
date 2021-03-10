92a0a6ce6e070e1365bb481e55a4f5690ceefaf2
dasistdangl <cd@dasistweb.de>
2021-03-10 15:18:03 +0100

    add new build workflow actions

--- build/generate-build-actions.php
+++ build/generate-build-actions.php
@@ -2,7 +2,7 @@
 
 $yml = 'name: Release dockware/##image##:*
 
-#only manuelly
+#only manually
 on:
   workflow_dispatch:
 
@@ -36,10 +36,37 @@ $template='
             password: ${{ secrets.DOCKERHUB_PASSWORD }}
     
         - name: Push to Docker Hub
-          run: php ./build/push_dockerhub.php ##image## ##tag##';
+          run: docker push dockware/##image##:##tag##';
 
 
-include(__DIR__ . '/_variables.inc.php');
+
+if (count($argv) < 2) {
+    throw new Exception('you have to provide the image name you want to build');
+}
+
+
+$image = $argv[1];
+$tag = null;
+$tags = [];
+if (count($argv) === 3) {
+    $tag = $argv[2];
+    if (trim($tag) !== '') {
+        $tags = [];
+        $tags[] = $tag;
+    }
+}
+
+if (count($tags) === 0) {
+    $folder = scandir(__DIR__ . '/../dist/images/' . $image);
+
+    foreach ($folder as $tag) {
+        if ($tag === '.' || $tag === '..') {
+            continue;
+        }
+        $tags[] = $tag;
+    }
+}
+
 
 $template = str_replace('##image##', $image, $template);
 $yml = str_replace('##image##', $image, $yml);
