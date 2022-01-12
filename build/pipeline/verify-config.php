1ea13a6ffe471463537e3c417ec0581ffcf86cac
boxblinkracer <boxblinkracer@gmx.net>
2022-01-12 14:48:26 +0100

    add docker layer count verification

--- build/pipeline/verify-config.php
+++ build/pipeline/verify-config.php
@@ -4,13 +4,47 @@ $swImage = $argv[1];
 $swTag = $argv[2];
 
 
-$svrUnitFile = __DIR__ . '/../../tests/svrunit/suites/' . $swImage . '/' . $swTag . '.xml';
-
-
-if (!file_exists($svrUnitFile)) {
-    echo "SVRUNIT XML File does not exist: " . $svrUnitFile . PHP_EOL;
+if (!isSVRUnitValid($swImage, $swTag)) {
+    echo "SVRUNIT XML File does not exist." . PHP_EOL;
     exit(1);
 }
 
 
+$layerCount = getLayersCount($swImage, $swTag);
+
+if ($layerCount >= 127) {
+    echo "DOCKER LAYER COUNT IS TOO HIGH: " . $layerCount . PHP_EOL;
+    exit(1);
+}
+
 echo "Configuration Valid!" . PHP_EOL;
+
+
+/**
+ * @param $swImage
+ * @param $swTag
+ * @return bool
+ */
+function isSVRUnitValid($swImage, $swTag)
+{
+    $svrUnitFile = __DIR__ . '/../../tests/svrunit/suites/' . $swImage . '/' . $swTag . '.xml';
+
+    return file_exists($svrUnitFile);
+}
+
+/**
+ * @param $swImage
+ * @param $swTag
+ * @return int
+ */
+function getLayersCount($swImage, $swTag)
+{
+    $dockerFile = __DIR__ . '/../../dist/images/' . $swImage . '/' . $swTag . '/Dockerfile';
+    $dockerFileContent = file_get_contents($dockerFile);
+
+    $runs = substr_count($dockerFileContent, "RUN ");
+    $adds = substr_count($dockerFileContent, "ADD ");
+    $copy = substr_count($dockerFileContent, "COPY ");
+
+    return $runs + $adds + $copy;
+}
