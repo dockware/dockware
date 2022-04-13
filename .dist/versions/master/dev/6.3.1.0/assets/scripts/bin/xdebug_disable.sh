9e2d0fdc98590351eb797c2bb70d52cb99a23100
Martin Weinmayr <mw@dasistweb.de>
2022-04-13 14:20:12 +0000

    Auto Commit from Github Workflow

--- .dist/versions/master/dev/6.3.1.0/assets/scripts/bin/xdebug_disable.sh
+++ .dist/versions/master/dev/6.3.1.0/assets/scripts/bin/xdebug_disable.sh
@@ -1,7 +1,7 @@
 echo "DOCKWARE: deactivating Xdebug..."
 
 #make sure we use the current running php version and not that one from the ENV
-PHP_VERSION_RUNNING=$(php -r "echo substr(phpversion(),0,3);")
+PHP_VERSION_RUNNING=$(php -r `echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;`)
 
 sudo mv /etc/php/${PHP_VERSION_RUNNING}/fpm/conf.d/20-xdebug.ini /etc/php/${PHP_VERSION_RUNNING}/fpm/conf.d/20-xdebug.ini_disabled  > /dev/null 2>&1 &
 sudo mv /etc/php/${PHP_VERSION_RUNNING}/cli/conf.d/20-xdebug.ini /etc/php/${PHP_VERSION_RUNNING}/cli/conf.d/20-xdebug.ini_disabled  > /dev/null 2>&1 &
