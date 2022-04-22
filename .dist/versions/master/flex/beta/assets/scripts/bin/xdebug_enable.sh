d562e77d9d09b011e54ae30fc3ecad7f9584fb72
dasistdangl <cd@dasistweb.de>
2022-04-22 12:55:12 +0000

    Auto Commit from Github Workflow

--- .dist/versions/master/flex/beta/assets/scripts/bin/xdebug_enable.sh
+++ .dist/versions/master/flex/beta/assets/scripts/bin/xdebug_enable.sh
@@ -1,7 +1,7 @@
 echo "DOCKWARE: activating Xdebug..."
 
 #make sure we use the current running php version and not that one from the ENV
-PHP_VERSION_RUNNING=$(php -r `echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;`)
+PHP_VERSION_RUNNING=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
 
 sudo mv /etc/php/${PHP_VERSION_RUNNING}/fpm/conf.d/20-xdebug.ini_disabled /etc/php/${PHP_VERSION_RUNNING}/fpm/conf.d/20-xdebug.ini  > /dev/null 2>&1 &
 sudo mv /etc/php/${PHP_VERSION_RUNNING}/cli/conf.d/20-xdebug.ini_disabled /etc/php/${PHP_VERSION_RUNNING}/cli/conf.d/20-xdebug.ini  > /dev/null 2>&1 &
