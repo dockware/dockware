6784b33fb3a875f6fbdb6262375ed136dc2b30e3
Martin Weinmayr <mw@dasistweb.de>
2023-01-10 15:53:21 +0100

    add tests

--- build/build-tests.php
+++ build/build-tests.php
@@ -1,10 +1,8 @@
 <?php
-
-include __DIR__ . '/TestPipelineBuilder.php';
-
+ini_set('DISPLAY_ERROR', 'E_ALL');
+include __DIR__.'/TestPipelineBuilder.php';
 
 buildTests();
-
 
 /**
  *
@@ -13,31 +11,31 @@ function buildTests()
 {
     $builder = new TestPipelineBuilder();
 
-    $manifest = file_get_contents(__DIR__ . '/../manifest.json');
+    $manifest = file_get_contents(__DIR__.'/../manifest.json');
     $manifestJson = json_decode($manifest, true);
 
     foreach ($manifestJson['images'] as $image => $variants) {
+        if (!is_dir(__DIR__.'/../tests/svrunit/suites/'.$image)) {
+            mkdir(__DIR__.'/../tests/svrunit/suites/'.$image);
+        }
 
         foreach ($variants as $variant) {
-
             $tag = explode(':', $variant)[1];
-
             $yml = $builder->buildJob(
-                $image . '-' . str_replace('.', '-', $tag),
+                $image.'-'.str_replace('.', '-', $tag),
                 $image,
-                $tag);
+                $tag
+            );
 
             if (empty($yml)) {
+                echo "\n not created for $image/$variant because there is no test content generated";
                 continue;
             }
 
             file_put_contents(
-                __DIR__ . '/../tests/svrunit/suites/' . $image . '/' . $tag . '.xml',
+                __DIR__.'/../tests/svrunit/suites/'.$image.'/'.$tag.'.xml',
                 $yml
             );
-
         }
-
-
     }
 }
