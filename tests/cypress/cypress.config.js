b6b9d8f8f3c1284925538bc37f03383b8d983240
boxblinkracer <boxblinkracer@gmx.net>
2023-02-24 16:05:23 +0100

    upgrade to cypress 12.6.0

---
+++ tests/cypress/cypress.config.js
@@ -0,0 +1,24 @@
+const {defineConfig} = require('cypress')
+
+module.exports = defineConfig({
+    chromeWebSecurity: false,
+    retries: {
+        "runMode": 3,
+        "openMode": 0
+    },
+    watchForFileChanges: false,
+    trashAssetsBeforeRuns: true,
+    screenshotOnRunFailure: true,
+    video: false,
+    videoCompression: 50,
+    e2e: {
+        experimentalSessionAndOrigin: true,
+        testIsolation: "strict",
+        experimentalWebKitSupport: true,
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return require('./cypress/plugins/index.js')(on, config)
+        },
+    },
+})
