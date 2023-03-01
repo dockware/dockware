74fc3db6ae0e72062eaa57a41621f50d1f9f1cff
dasistdangl <cd@dasistweb.de>
2023-03-01 16:39:52 +0000

    Auto Commit from Github Workflow

--- .dist/versions/master/contribute/latest/assets/shopware6/files/update_domain.sh
+++ .dist/versions/master/contribute/latest/assets/shopware6/files/update_domain.sh
@@ -2,3 +2,6 @@ mysql -h 127.0.0.1 -uroot -proot shopware < /var/www/scripts/shopware6/create_ht
 
 mysql -h 127.0.0.1 -uroot -proot -e "use shopware; UPDATE sales_channel_domain SET url = 'http://${SHOP_DOMAIN}' WHERE url like 'http://%';"
 mysql -h 127.0.0.1 -uroot -proot -e "use shopware; UPDATE sales_channel_domain SET url = 'https://${SHOP_DOMAIN}' WHERE url like 'https://%';"
+
+sed -i "/APP_URL=/g" /var/www/html/.env
+echo "APP_URL=http://${SHOP_DOMAIN}" >> /var/www/html/.env
