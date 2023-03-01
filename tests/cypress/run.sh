83a6634de324ccf16729b318151ce00deb89af8c
dasistdangl <cd@dasistweb.de>
2023-03-01 21:51:39 +0100

    fix cypress tests circleci

--- tests/cypress/run.sh
+++ tests/cypress/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [[ $IMG == "flex" ]]; then
-  make run-flex url=http://localhost
+  make run-flex url=$URL
 elif [[ $IMG == "essentials" ]]; then
-  make run-essentials url=http://localhost
+  make run-essentials url=$URL
 elif [[ $IMG == "play" && $TAG == "latest" ]]; then
-  make run6 url=http://localhost shopware=6.4.20.0
+  make run6 url=$URL shopware=6.4.20.0
 elif [[ $IMG == "dev" && $TAG == "latest" ]]; then
-  make run6 url=http://localhost shopware=6.4.20.0
+  make run6 url=$URL shopware=6.4.20.0
 elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc1" ]]; then
-  make run6 url=http://localhost shopware=6.5.0.0
+  make run6 url=$URL shopware=6.5.0.0
 elif [[ $TAG == "6."* ]]; then
-  make run6 url=http://localhost shopware=$TAG
+  make run6 url=$URL shopware=$TAG
 elif [[ $TAG == "5."* ]]; then
-  make run5 url=http://localhost shopware=$TAG
+  make run5 url=$URL shopware=$TAG
 else
   echo "NO CYPRESS CONFIGURATION FOUND FOR IMAGE"
   exit 1
