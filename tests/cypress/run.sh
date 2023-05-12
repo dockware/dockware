319ad8113e059acd922ed1ba834da98a706c37d9
Martin Weinmayr <mw@dasistweb.de>
2023-05-12 08:27:24 +0200

    try to fix cypress

--- tests/cypress/run.sh
+++ tests/cypress/run.sh
@@ -2,19 +2,13 @@
 
 if [[ $IMG == "flex" ]]; then
   make run-flex url=$URL
-elif [[ $IMG == "essentials" ]]; then
+elif [[ $IMG == "base_play" || $IMG == "base_dev" ]]; then
+  make run-flex url=$URL
+elif [[ $IMG == "essentials" || $IMG == "essentials_play" ]]; then
   make run-essentials url=$URL
 elif [[ $IMG == "play" && $TAG == "latest" ]]; then
-  make run6 url=$URL shopware=6.4.20.0
+  make run6 url=$URL shopware=6.5.0.0
 elif [[ $IMG == "dev" && $TAG == "latest" ]]; then
-  make run6 url=$URL shopware=6.4.20.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc1" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc2" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc3" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc4" ]]; then
   make run6 url=$URL shopware=6.5.0.0
 elif [[ $TAG == "6."* ]]; then
   make run6 url=$URL shopware=$TAG
