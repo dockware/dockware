243a1712a6895f8c2cb90f5327d629b08aee6862
dasistdangl <cd@dasistweb.de>
2023-07-05 08:54:46 +0200

    debug pipeline

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-curl -X POST https://circleci.com/api/v2/project/github/dockware/dockware/pipeline \
-  -H "Content-Type: application/json" \
-  -H "Circle-Token: $CIRCLECI_KEY" \
-  -d "{
-        \"parameters\":
-            {
-                \"shopwareVersion\": \"$SW_VERSION\",
-                \"setLatest\" : $SET_LATEST
-            }
-      }"
+# export to temporary file to debug values
+cat <<EOT >> body.json
+{
+  "parameters":
+      {
+          "shopwareVersion": "$(SW_VERSION)",
+          "setLatest" : $(SET_LATEST)
+      }
+}
+EOT
 
+cat body.json
+
+curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $CIRCLECI_KEY" https://circleci.com/api/v2/project/github/dockware/dockware/pipeline
