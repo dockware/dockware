7253c49df28ad73fa68dc5edbbdafe324843dbf5
dasistdangl <cd@dasistweb.de>
2023-07-05 08:57:45 +0200

    fix pipe

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -5,12 +5,10 @@ cat <<EOT >> body.json
 {
   "parameters":
       {
-          "shopwareVersion": "$(SW_VERSION)",
-          "setLatest" : $(SET_LATEST)
+          "shopwareVersion": "$2",
+          "setLatest" : $3
       }
 }
 EOT
 
-cat body.json
-
-curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $CIRCLECI_KEY" https://circleci.com/api/v2/project/github/dockware/dockware/pipeline
+curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $1" https://circleci.com/api/v2/project/github/dockware/dockware/pipeline
