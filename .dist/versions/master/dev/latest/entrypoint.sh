6c4fdf3366ddd53368b6abd9d3596705717aeaeb
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2023-11-22 14:52:06 +0000

    Auto Commit from Github Workflow

--- .dist/versions/master/dev/latest/entrypoint.sh
+++ .dist/versions/master/dev/latest/entrypoint.sh
@@ -45,38 +45,6 @@ sudo ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
 sudo dpkg-reconfigure -f noninteractive tzdata
 echo "-----------------------------------------------------------"
 
-if [ $FILEBEAT_ENABLED = 1 ]; then
-   echo "DOCKWARE: activating Filebeat..."
-   sudo service filebeat start --strict.perms=false
-   echo "-----------------------------------------------------------"
-fi
-
-# checks if a different username is set in ENV and create if its not existing yet
-if [ $SSH_USER != "not-set" ] && (! id -u "${SSH_USER}" >/dev/null 2>&1 ); then
-    echo "DOCKWARE: creating additional SSH user...."
-    # create a custom ssh user for our provided settings
-    sudo adduser --disabled-password --uid 8888 --gecos "" --ingroup www-data $SSH_USER
-    sudo usermod -a -G sudo $SSH_USER
-    sudo usermod -m -d /var/www $SSH_USER | true
-    sudo echo "${SSH_USER}:${SSH_PWD}" | sudo chpasswd
-    sudo sed -i "s/${SSH_USER}:x:8888:33:/${SSH_USER}:x:33:33:/g" /etc/passwd
-    # add sudo without password
-    # write user to file cause we loos the var as we executing as root and get a new shell
-    sudo echo "${SSH_USER}" >> /tmp/user.name
-    sudo -u root sh -c 'echo "Defaults:$(cat /tmp/user.name) !requiretty" >> /etc/sudoers'
-    sudo rm -rf /tmp/user.name
-    # disable original ssh access
-    sudo usermod -s /bin/false dockware
-    # allow ssh in sshd_config
-    sudo sed -i "s/AllowUsers dockware/AllowUsers ${SSH_USER}/g" /etc/ssh/sshd_config
-    echo "-----------------------------------------------------------"
-fi
-
-# start the SSH service with the latest setup
-echo "DOCKWARE: restarting SSH service...."
-sudo service ssh restart
-echo "-----------------------------------------------------------"
-
 echo "DOCKWARE: starting MySQL...."
 # somehow its necessary to set permissions, because
 # sometimes they get lost :)
@@ -89,29 +57,6 @@ fi
 sudo chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 sudo service mysql start;
 
-if [ $MYSQL_USER != "not-set" ] && [ $MYSQL_PWD != "not-set" ]; then
-    echo "DOCKWARE: creating new MySQL user...."
-    # -----------------------------------
-    # Shopware users triggers. the DEFINER does also need to be changed to our new user
-    # otherwise problems like "product cant be created in admin" will occur.
-    # the only solution is to export the triggers, replace the DEFINER and import it again.
-    sudo mysqldump -P 3306 -h localhost -u root -p"root" --triggers --add-drop-trigger --no-create-info --no-data --no-create-db --skip-opt shopware > /tmp/triggers.sql
-    sudo sed -i 's/DEFINER=`root`@`%`/DEFINER=`'$MYSQL_USER'`@`%`/g' /tmp/triggers.sql
-    sudo mysql --user=root --password=root shopware < /tmp/triggers.sql
-    sudo rm -rf /tmp/triggers.sql
-    # -----------------------------------
-    # block remote access for root user
-    sudo mysql --user=root --password=root -e "DELETE FROM mysql.user WHERE User='root' AND Host='%';";
-    # -----------------------------------
-    # add new user and grant privileges
-    sudo mysql --user=root --password=root -e "CREATE USER IF NOT EXISTS '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PWD"';";
-    sudo mysql --user=root --password=root -e "use mysql; update user set host='%' where user='$MYSQL_USER';";
-    sudo mysql --user=root --password=root -e "GRANT ALL PRIVILEGES ON *.* TO '"$MYSQL_USER"'@'%'";
-    # -----------------------------------
-    # apply and flush privileges
-    sudo mysql --user=root --password=root -e "FLUSH PRIVILEGES;";
-    echo "-----------------------------------------------------------"
-fi
 echo "-----------------------------------------------------------"
 
 echo "DOCKWARE: starting mailcatcher...."
@@ -132,33 +77,6 @@ echo "DOCKWARE: switching to PHP ${PHP_VERSION}..."
 cd /var/www && make switch-php version=${PHP_VERSION}
 sudo service apache2 stop
 echo "-----------------------------------------------------------"
-
-if [ $XDEBUG_ENABLED = 1 ]; then
-   sh /var/www/scripts/bin/xdebug_enable.sh
- else
-   sh /var/www/scripts/bin/xdebug_disable.sh
-fi
-
-if [ $TIDEWAYS_KEY != "not-set" ]; then
-    echo "DOCKWARE: activating Tideways...."
-    sudo sed -i 's/__DOCKWARE_VAR_TIDEWAYS_ENV__/'${TIDEWAYS_ENV}'/g' /etc/default/tideways-daemon
-    sudo sed -i 's/__DOCKWARE_VAR_TIDEWAYS_API_KEY__/'${TIDEWAYS_KEY}'/g' /etc/php/$PHP_VERSION/fpm/conf.d/20-tideways.ini
-    sudo sed -i 's/__DOCKWARE_VAR_TIDEWAYS_API_KEY__/'${TIDEWAYS_KEY}'/g' /etc/php/$PHP_VERSION/cli/conf.d/20-tideways.ini
-    sudo service tideways-daemon start
-    echo "-----------------------------------------------------------"
-fi
-
-if [[ ! -z "$NODE_VERSION" ]]; then
-   echo "DOCKWARE: switching to Node ${NODE_VERSION}..."
-   nvm alias default ${NODE_VERSION}
-   # now make sure to at least have node and npm as sudo
-   # nvm itself is not possible by design
-   sudo rm -f /usr/local/bin/node
-   sudo rm -f /usr/local/bin/npm
-   sudo ln -s "$(which node)" "/usr/local/bin/node"
-   sudo ln -s "$(which npm)" "/usr/local/bin/npm"
-   echo "-----------------------------------------------------------"
-fi
 
 if [ $SHOP_DOMAIN != "localhost" ]; then
   # update our domain. this means we can use the
@@ -262,23 +180,6 @@ echo ""
 # ------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------------------------
-
-if [[ -z "${BUILD_PLUGIN}" ]]; then
-    echo ""
-else
-    echo "STARTING IN PLUGIN BUILDING MODE...."
-    echo "DOCKWARE WILL NOW BUILD YOUR PLUGIN AND EXIT THE CONTAINER AFTERWARDS"
-    echo ""
-    # if we are in the plugin build mode, then
-    # make sure to run our commands to  install and activate it.
-    # afterwards build the javascript
-        cd /var/www/html && php bin/console plugin:refresh && \
-    cd /var/www/html && php bin/console plugin:install --activate "${BUILD_PLUGIN}"
-    cd /var/www/html && ./bin/build-js.sh
-# set the CI mode to ON, so that our container exists after
-# installing and building our plugin
-    export DOCKWARE_CI=1
-    fi
 
 # used to inject the custom build script of
 # plugins in dockware/dev
