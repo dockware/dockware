6d8c3d5bfe8e3a5e05359e80fbbf104e983357ca
dasistdangl <cd@dasistweb.de>
2024-01-19 12:35:48 +0100

    fix wrong cypress tests for sw6

--- tests/cypress/cypress/e2e/sw6/storefront/category/category.cy.js
+++ tests/cypress/cypress/e2e/sw6/storefront/category/category.cy.js
@@ -2,8 +2,8 @@ it('Categories show products', () => {
 
     cy.visit('/');
 
-    cy.get('[href="http://localhost/Clothing/"] > .main-navigation-link-text > span').click();
-
+    cy.get('nav.main-navigation-menu a[href*="Clothing"]').click();
+ 
     // check if we see a product box
     cy.get(':nth-child(1) > .card > .card-body').should('be.visible');
 })
