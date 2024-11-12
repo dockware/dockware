079bf1d8a2273df22007ecdfdbd875a010b4e6b6
Vitalij Mik <BlackScorp@users.noreply.github.com>
2024-11-12 15:53:22 +0000

    Auto Commit from Github Workflow

---
+++ .dist/versions/master/dev/6.6.8.0/assets/shopware6/DockwareSamplePlugin/tests/Services/SampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DockwareSamplePlugin\Tests\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    
+    /**
+     * This is a sample to get you going with unit tests.
+     * We love testing, and hopefully you too - have fun!
+     */
+    public function testSample()
+    {
+        $this->assertEquals(true, true);
+    }
+    
+}
