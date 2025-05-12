1afc70c486590d0c77fb9f67c14b5473b316d21c
Wim Wenigerkind <159561025+wimwenigerkind@users.noreply.github.com>
2025-05-12 12:10:38 +0000

    Auto Commit from Github Workflow

---
+++ .dist/versions/master/play/6.6.10.4/assets/shopware6/files/set_currency.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once '/var/www/html/vendor/autoload.php';
+
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\FetchMode;
+
+$newCurrency = $argv[1];
+
+# ----------------------------------------------------------------------------
+$connection = DriverManager::getConnection(
+[
+    "driver" => "pdo_mysql",
+    "host" => "127.0.0.1",
+    "port" => 3306,
+    "user" => "root",
+    "password" => "root",
+    "dbname" => "shopware",
+    "charset" => "utf8mb4"
+]
+);
+
+$connection->executeQuery('USE `shopware`');
+# ----------------------------------------------------------------------------
+
+$row = $connection->executeQuery("SELECT factor FROM currency WHERE iso_code = '" . $newCurrency . "'")->fetch();
+$factor = $row['factor'];
+
+if ((double)$factor === 1.0)
+{
+    # already default
+    return;
+}
+
+$sql = "
+START TRANSACTION;
+
+SET @defaultID = (SELECT id FROM currency WHERE iso_code = 'EUR');
+SET @otherID = (SELECT id FROM currency WHERE iso_code = '" . $newCurrency . "');
+UPDATE currency SET id = 'temp' WHERE iso_code = 'EUR';
+UPDATE currency SET id = @defaultID WHERE iso_code = '" . $newCurrency . "';
+UPDATE currency SET id = @otherID WHERE iso_code = 'EUR';
+
+SET @fixFactor = (SELECT 1/factor FROM currency WHERE iso_code = '" . $newCurrency . "');
+UPDATE currency SET factor = IF(iso_code = '" . $newCurrency . "', 1, factor * @fixFactor);
+
+COMMIT;
+";
+
+$connection->executeQuery($sql);
