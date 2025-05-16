c197d01bc55aefb3a9adfb1d448981f3b2bea1cb
Christian <boxblinkracer@gmx.net>
2025-05-16 10:05:02 +0200

    improve cypress script

--- tests/cypress/run.sh
+++ tests/cypress/run.sh
@@ -1,55 +1,25 @@
 #!/bin/bash
 
+# remove -rcX suffix if present
+TAG_CLEANED=${TAG%%-rc*}
+
+if [[ $TAG_CLEANED == 6.7.* ]]; then
+  echo "Skip tests for 6.7.x"
+  exit 0
+fi
+
 if [[ $IMG == "flex" ]]; then
   make run-flex url=$URL
 elif [[ $IMG == "essentials" ]]; then
   make run-essentials url=$URL
 elif [[ $IMG == "play" && $TAG == "latest" ]]; then
-  make run6 url=$URL shopware=6.4.20.0
+  make run6 url=$URL shopware=6.6.0.0
 elif [[ $IMG == "dev" && $TAG == "latest" ]]; then
-  make run6 url=$URL shopware=6.4.20.0
-elif [[ $TAG == "6.7.0.0-rc1" ]]; then
-  echo "Skip tests"
-  exit 0
-elif [[ $TAG == "6.7.0.0-rc1" ]]; then
-  echo "Skip tests"
-  exit 0
-elif [[ $TAG == "6.7.0.0-rc2" ]]; then
-    echo "Skip tests"
-    exit 0
-elif [[ $TAG == "6.7.0.0-rc2" ]]; then
-    echo "Skip tests"
-    exit 0
-elif [[ $TAG == "6.7.0.0-rc3" ]]; then
-    echo "Skip tests"
-    exit 0
-elif [[ $TAG == "6.7.0.0-rc3" ]]; then
-    echo "Skip tests"
-    exit 0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc6" ]]; then
   make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc5" ]]; then
-  make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc4" ]]; then
-  make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc3" ]]; then
-  make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc2" ]]; then
-  make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.6.0.0-rc1" ]]; then
-  make run6 url=$URL shopware=6.6.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc1" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc2" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc3" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $IMG == "dev" && $TAG == "6.5.0.0-rc4" ]]; then
-  make run6 url=$URL shopware=6.5.0.0
-elif [[ $TAG == "6."* ]]; then
-  make run6 url=$URL shopware=$TAG
-elif [[ $TAG == "5."* ]]; then
-  make run5 url=http://localhost shopware=$TAG
+elif [[ $TAG_CLEANED == 6.* ]]; then
+  make run6 url=$URL shopware=$TAG_CLEANED
+elif [[ $TAG_CLEANED == 5.* ]]; then
+  make run5 url=http://localhost shopware=$TAG_CLEANED
 else
   echo "NO CYPRESS CONFIGURATION FOUND FOR IMAGE"
   exit 1
